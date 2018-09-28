4c00673a95b9c28ce730a5e2f2a8274302810586
foureverhh <foureverhh@gmail.com>
2018-09-28 11:11:32 +0200

    Working on sniper rifle

---
+++ Sniper/Assets/Scripts/ScopeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeController : MonoBehaviour {
+
+    public Animator animator;
+    private int scopedHash = Animator.StringToHash("Scoped");
+    private bool isScoped = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isScoped = ! isScoped;
+            animator.SetBool(scopedHash,isScoped);
+        }
+    }
+}

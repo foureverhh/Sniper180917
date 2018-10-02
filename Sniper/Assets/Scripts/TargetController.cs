79bca390e0a04506bca866f647f7d176b0304fb4
foureverhh <foureverhh@gmail.com>
2018-10-02 11:41:02 +0200

    forgot to save scene on last push

--- Sniper/Assets/Scripts/TargetController.cs
+++ Sniper/Assets/Scripts/TargetController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargeHeartController : MonoBehaviour {
+public class TargetController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
